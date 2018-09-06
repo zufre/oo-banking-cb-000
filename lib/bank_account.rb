c2841509f19e6bf51febcb226f9aa25abfffab0b
Cedric David <cedric.david@btopenworld.com>
2018-09-06 05:48:36 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,14 +1,14 @@
 class BankAccount
-  attr_reader :name
+  attr_reader :name :status
   def initialize(name)
     @name = name
     @balance = 1000
     @status = 'open'
-    
+
   end
 
   def balance
     @balance
   end
-  
+
 end
