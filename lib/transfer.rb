9ac828458f878a09df00ab2554e1d95ee7c5b60c
Cedric David <cedric.david@btopenworld.com>
2018-09-06 06:42:44 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,9 +30,7 @@ class Transfer
       self.status = "reversed"
     else
       self.status = "rejected"
-    "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
+    end
   end
-
-  end
-
 end
