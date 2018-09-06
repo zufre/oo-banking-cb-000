5f80e42de7d58beccd8d16abc330a271bd41392c
Cedric David <cedric.david@btopenworld.com>
2018-09-06 06:16:10 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,8 @@ class Transfer
 
   def execute_transaction
     sender.balance -= @amount
-    receiver.balance ++ @amount
+    receiver.balance += @amount
+    self.status = "complete"
   end
 
 end
