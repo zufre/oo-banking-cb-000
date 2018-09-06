5215040e91f9353336ab8a59e0b4b21c1e25f5b8
Cedric David <cedric.david@btopenworld.com>
2018-09-06 06:37:02 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,8 +19,9 @@ class Transfer
       receiver.balance += @amount
       @status = "complete"
     else
-      "Transaction rejected. Please check your account balance."
       self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+      
     end
   end
 end
