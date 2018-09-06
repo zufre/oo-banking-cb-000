762e2d0bb232f93488670ac16edaca2b56108bfc
Cedric David <cedric.david@btopenworld.com>
2018-09-06 06:26:12 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.valid?
+    if !sender.valid?
       "Transaction rejected. Please check your account balance."
     elsif @status == "pending" 
       sender.balance -= @amount
